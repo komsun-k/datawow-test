30bd0940f0dc59c16c59ddfe507112481e0e42e2
Komsun K <kk_workplace@hotmail.com>
2021-07-28 23:55:13 +0700

    Setup Vue.js environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const vue = require('./loaders/vue')
+
+environment.loaders.append('vue', vue)
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+environment.plugins.append(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
 
 module.exports = environment
